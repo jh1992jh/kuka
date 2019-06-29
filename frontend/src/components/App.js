768edb534427320893a7b4d3808137cd604956ec
jaakko <jaakko.kuurne@gmail.com>
2019-06-29 21:50:56 +0300

    added bootstrap (css only)

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link, BrowserRouter as Router, Route } from 'react-router-dom';
 import { Login } from './Login';
 import '../assets/App.css';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 class App extends Component {
   render() {
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -4,8 +4,15 @@ export class Login extends Component {
     render() {
         return (
             <form>
-                <input type="text" />
-                <input type="password" />
+                <div className="form-group">
+                    <label htmlFor="exampleInputEmail1">Email address</label>
+                    <input className="form-control" id="exampleInputEmail1" type="email" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="exampleInputEmail1">Password</label>
+                    <input className="form-control" id="exampleInputEmail1" type="password" />
+                </div>
+
             </form>
         );
     }
